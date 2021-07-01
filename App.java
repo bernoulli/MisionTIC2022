d7fbc8fc1c3d1ef64f9da702a3517a071ba0e802
Bernardo Cuervo <bcuervo@utp.edu.co>
2021-07-01 12:33:50 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,71 @@
+
+
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hola, mundo!");
+
+        var sc = new Scanner(System.in); 
+        System.out.println("Por favor ingrese su nombre: "); 
+        var nombre = sc.next(); 
+        System.out.print("Holas " + nombre + "!!");
+
+        /*
+        int x = 0;
+        x = 3;
+        int[] numeros = {1, 2, 3};
+        System.out.println(numeros[1]);
+
+
+
+        //x = 3;
+
+        String hola = "hola";
+        //String static = "hola";
+        //char y = 'a';
+
+        //y = (char) 'a';
+
+        System.out.println(x);
+
+        var f = 4.3;
+        var Example = "ejemplo";
+        //var static = "hola";
+        */
+
+    }
+}
+
+        /*
+        var sc = new Scanner(System.in); 
+        System.out.println("Por favor ingrese su nombre: "); 
+        var nombre = sc.nextLine(); 
+        System.out.println("Hola " + nombre + "!");
+        /*
+
+        //ciclo for
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+
+        //Ciclo while
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+
+        //Ciclo do while
+        int j = 0;
+        do {
+        System.out.println(i);
+        j++;
+        }
+        while (j < 5);
+        */
+    //}
+//}
+
